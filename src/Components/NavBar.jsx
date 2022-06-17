38619d60811697787c3f079c44b8268fc06b9144
JorgeVallone <albertovallone33@gmail.com>
2022-06-17 02:11:21 -0300

    subiendo el nav bar a Git

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -12,9 +12,9 @@ const NavBar = () =>
            <li><a href="#">Nosotros</a></li>
            <li><a href="#">Servicios</a>
              <ul className='navUlistSubmenu'> 
-               <li><a href="">Envio a Domicilio</a></li>
-               <li><a href="">Envio a Sucursal</a></li>
-               <li><a href="">Retiro en Delivery Point</a></li>
+               <li><a href="#">Envio a Domicilio</a></li>
+               <li><a href="#">Envio a Sucursal</a></li>
+               <li><a href="#">Retiro en Delivery Point</a></li>
              </ul>
             </li>
            <li><a href="">Conctactos</a></li>
