483fbceccb7336a7d16f8978584e66da033f4603
Alberto VAllone <albertovallone33@gmail.com>
2022-06-16 21:27:24 -0300

    Nav Bar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import NavBar from './Components/NavBar';
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+    <NavBar/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,34 @@
+import react from 'react';
+import './NavBar.css';
+
+const NavBar = () =>
+{
+
+    return(
+       
+        <nav className='navFather'>
+            <ul className='navUnorderedList'>
+           <li><a href="">Inicio</a></li>
+           <li><a href="">Nosotros</a></li>
+           <li><a href="">Servicios</a>
+             <ul className='navUlistSubmenu'> 
+               <li><a href="">Envio a Domicilio</a></li>
+               <li><a href="">Envio a Sucursal</a></li>
+               <li><a href="">Retiro en Delivery Point</a></li>
+             </ul>
+            </li>
+           <li><a href="">Conctactos</a></li>
+
+        
+
+
+            </ul>
+
+        </nav>
+
+
+
+    )
+
+}
+export default NavBar
